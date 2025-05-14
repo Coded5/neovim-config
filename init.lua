7572c60c94f108a8dc6aae89ab18af5e17ba8fc1
Nutdanai Sikkhakul <phoenixanimation24@gmail.com>
2025-05-14 23:57:09 +0700

    add vim-tmux and vim golf 4 fun

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@ vim.opt.mouse = "a"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.wrap = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.breakindent = true
 vim.opt.termguicolors = true
@@ -22,41 +22,44 @@ vim.opt.completeopt = { "menu", "menuone", "noselect" }
 local lazy = {}
 
 function lazy.install(path)
-	if not vim.loop.fs_stat(path) then
-		print("Installing lazy.nvim....")
-		vim.fn.system({
-			"git",
-			"clone",
-			"--filter=blob:none",
-			"https://github.com/folke/lazy.nvim.git",
-			"--branch=stable", -- latest stable release
-			path,
-		})
-	end
+    if not vim.loop.fs_stat(path) then
+        print("Installing lazy.nvim....")
+        vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            path,
+        })
+    end
 end
 
 function lazy.setup(plugins)
-	if vim.g.plugins_ready then
-		return
-	end
+    if vim.g.plugins_ready then
+        return
+    end
 
-	-- You can "comment out" the line below after lazy.nvim is installed
-	lazy.install(lazy.path)
+    -- You can "comment out" the line below after lazy.nvim is installed
+    lazy.install(lazy.path)
 
-	vim.opt.rtp:prepend(lazy.path)
+    vim.opt.rtp:prepend(lazy.path)
 
-	require("lazy").setup(plugins, lazy.opts)
-	vim.g.plugins_ready = true
+    require("lazy").setup(plugins, lazy.opts)
+    vim.g.plugins_ready = true
 end
 
 lazy.path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 lazy.opts = {}
 
 lazy.setup({
-	spec = {
-		{ import = "plugins" },
-	},
-	checker = { enabled = true, notify = false },
+    spec = {
+        { import = "plugins" },
+    },
+    checker = { enabled = true, notify = false },
+    rocks = {
+        hererocks = true,
+    }
 })
 
 -- lazy.setup(require("plugins"))
--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			vim.g.loaded_netrw = 1
-			vim.g.loaded_netrwPlugin = 1
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
 
-			require("nvim-tree").setup({
-				view = {
-					width = 50,
-					side = "right",
-				},
-			})
-		end,
-	},
+      require("nvim-tree").setup({
+        view = {
+          width = 35,
+          side = "right",
+        },
+      })
+    end,
+  },
 }
