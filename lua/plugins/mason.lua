c0cfbaa688d0649a3475cc746268fd55ebf81c2b
Nutdanai Sikkhakul <phoenixanimation24@gmail.com>
2025-08-17 14:53:40 +0700

    Add mason installer tool

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,36 +1,59 @@
 return {
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            -- ensure_installed = {
-            --     "lua_ls",
-            --     "pyright",
-            --     "rust_analyzer" ,
-            --     "jdtls",
-            --     "eslint",
-            --     "ts_ls",
-            --     "html",
-            --     "cssls",
-            --     "clangd"
-            -- }
-        },
-        dependencies = {
-            { 
-                "mason-org/mason.nvim",
-                opts = {
-                    ui = {
-                        icons = {
-                            package_installed = "✓",
-                            package_pending = "➜",
-                            package_uninstalled = "✗"
-                        }
-                    }
-                } 
-            },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            require('configs.lsp')
-        end
-    },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			-- ensure_installed = {
+			--     "lua_ls",
+			--     "pyright",
+			--     "rust_analyzer" ,
+			--     "jdtls",
+			--     "eslint",
+			--     "ts_ls",
+			--     "html",
+			--     "cssls",
+			--     "clangd"
+			-- }
+		},
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("configs.lsp")
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_isntalled = {
+				"clangd",
+				"cmake-language-server",
+				"css-lsp",
+				"emmet-language-server",
+				"html-lsp",
+				"java-debug-adapter",
+				"jdtls",
+				"lua-language-server",
+				"prettier",
+				"prettierd",
+				"rust-analyzer",
+				"stylua",
+				"typescript-language-server",
+			},
+			integrations = {
+				["mason-lspconfig"] = true,
+			},
+		},
+	},
 }
