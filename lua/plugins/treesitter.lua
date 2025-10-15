fc143fb2f7c1e9400d7b1c6add3579d636ca52c8
Nutdanai Sikkhakul <phoenixanimation24@gmail.com>
2025-10-15 19:46:30 +0700

    Fix treesitter sometimes not working

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,28 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        -- opts = {
-        --     ensure_installed = {
-        --         "c", "lua", "vim", "vimdoc", "java", "javascript", "typescript", "html", "css", "rust"
-        --     },
-        -- },
-        lazy = false,
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+					"java",
+					"rust",
+					"svelte",
+					"vimdoc",
+					"vim",
+					"c",
+				},
+				sync_install = false,
+				auto_install = true,
+				ignore_install = {},
+				highlight = {
+					enable = true,
+				},
+			})
+		end,
+		lazy = false,
+	},
 }
