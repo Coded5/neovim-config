dcfb0a3f1486dcf602865736a6dd5029be607a64
Nutdanai Sikkhakul <phoenixanimation24@gmail.com>
2025-10-29 20:12:14 +0700

    Add harpoon

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -42,6 +42,39 @@ local on_attach = function(_, bufnr)
 	map("n", "<leader>d", vim.diagnostic.open_float, { buffer = bufnr, desc = "Show diagnostic" })
 end
 
+-- Harpoon
+
+local harpoon = require("harpoon")
+
+map("n", "<leader>a", function()
+	harpoon:list():add()
+	print("Added to harpoon list")
+end, { desc = "Add to harpoon list" })
+map("n", "<leader>A", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end, { desc = "Show harpoon list" })
+
+-- map("n", "<C-h>", function()
+-- 	harpoon:list():select(1)
+-- end)
+-- map("n", "<C-t>", function()
+-- 	harpoon:list():select(2)
+-- end)
+-- map("n", "<C-n>", function()
+-- 	harpoon:list():select(3)
+-- end)
+-- map("n", "<C-s>", function()
+-- 	harpoon:list():select(4)
+-- end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+map("n", "<C-S-P>", function()
+	harpoon:list():prev()
+end)
+map("n", "<C-S-N>", function()
+	harpoon:list():next()
+end)
+
 return {
 	on_attach = on_attach,
 }
