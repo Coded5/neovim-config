202120c07c4f6bb8b1b1b37e7cf37cabe4f881d3
Coded5 <phoenixanimation24@gmail.com>
2026-01-31 10:01:12 +0700

    prolog lsp filetype

--- lua/configs/lsp.lua
+++ lua/configs/lsp.lua
@@ -1,6 +1,14 @@
 vim.lsp.config("svelte", require("configs.lsp.svelte"))
 
+vim.lsp.enable("prolog")
+
 vim.diagnostic.config({
 	virtual_text = true,
 	virtual_lines = false,
+})
+
+vim.filetype.add({
+	extension = {
+		pl = "prolog",
+	},
 })
